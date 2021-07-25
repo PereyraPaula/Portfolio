85c6347572361d506595770156e1f509e2e6be01
Paula Pereyra <paulapereyrallorens@gmail.com>
2021-07-25 11:31:04 -0300

    Updates in the project section cards and other corrections.

--- js/script.js
+++ js/script.js
@@ -20,3 +20,21 @@ function toggleMenu(){
 		menu.classList.add('active')
 	}
 }
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
